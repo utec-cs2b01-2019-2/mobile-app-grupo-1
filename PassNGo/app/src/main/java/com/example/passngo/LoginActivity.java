3227bfb22c6a757d5ea58b4368a3381b4108fcc3
Mauricio-Bernuy <54604331+Mauricio-Bernuy@users.noreply.github.com>
2019-12-05 15:27:46 -0500

    setting files

---
+++ PassNGo/app/src/main/java/com/example/passngo/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.example.passngo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public Activity getActivity(){
+        return this;
+    }
+
+    public void onBtnLoginClicked(View view){
+        //Toast.makeText( this, "Logging In", Toast.LENGTH_SHORT).show();
+        EditText txtUsername = (EditText)findViewById(R.id.InputEmail);
+        EditText txtPassword = (EditText)findViewById(R.id.InputPassword);
+        String email = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        HashMap<String, String> message = new HashMap<>();
+        message.put("email", email);
+        message.put("password", password);
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                "http://10.0.2.2:8000/auth",
+                new JSONObject(message),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        showMessage("Authorized!");
+                        try {
+                            String email = response.getString("email");
+                            int id = response.getInt("id");
+                            goToContactsActivity(id, email);
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        showMessage("Unauthorized!");
+                    }
+                });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    private void goToContactsActivity(int id, String email){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("id", id);
+        intent.putExtra("email", email);
+        startActivity(intent);
+
+    }
+
+}
